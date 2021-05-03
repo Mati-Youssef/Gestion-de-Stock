562c4eb398e892aa782aa4da97218c1666f63166
khalid fifel <77207264+khalid2221@users.noreply.github.com>
2021-05-03 23:39:32 +0000

    base de donner de lUtilisateur et de client

---
+++ base de donner/gestion_stock.sql
@@ -0,0 +1,173 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 04, 2021 at 01:36 AM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `gestion_stock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `client`
+--
+
+CREATE TABLE `client` (
+  `id_client` int(40) NOT NULL,
+  `nom_prenom` varchar(60) NOT NULL,
+  `cin` varchar(30) NOT NULL,
+  `N_télé` int(40) NOT NULL,
+  `N_fix` int(40) NOT NULL,
+  `nom_de_magazine` varchar(60) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `client`
+--
+
+INSERT INTO `client` (`id_client`, `nom_prenom`, `cin`, `N_télé`, `N_fix`, `nom_de_magazine`) VALUES
+(1, 'khalid fifel', 'PA248450', 649459004, 2147483647, 'ward dades');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `entrer`
+--
+
+CREATE TABLE `entrer` (
+  `id_de_preduit` int(40) NOT NULL,
+  `id_de_categourie` varchar(40) NOT NULL,
+  `nom_de_preduit` varchar(40) NOT NULL,
+  `date_d_entrer` date NOT NULL,
+  `date_d_experation` date NOT NULL,
+  `quantité` int(40) NOT NULL,
+  `prix` double NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `entrer`
+--
+
+INSERT INTO `entrer` (`id_de_preduit`, `id_de_categourie`, `nom_de_preduit`, `date_d_entrer`, `date_d_experation`, `quantité`, `prix`) VALUES
+(1, '761', 'zit argan', '2021-05-03', '2021-05-11', 1, 16.6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sortie`
+--
+
+CREATE TABLE `sortie` (
+  `id_de_sortie` int(40) NOT NULL,
+  `id_de_preduit` int(40) NOT NULL,
+  `id_de_user` int(40) NOT NULL,
+  `id_de_client` int(40) NOT NULL,
+  `Prix_de_sortie` double NOT NULL,
+  `quantité` int(40) NOT NULL,
+  `date_de_sortie` date NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `sortie`
+--
+
+INSERT INTO `sortie` (`id_de_sortie`, `id_de_preduit`, `id_de_user`, `id_de_client`, `Prix_de_sortie`, `quantité`, `date_de_sortie`) VALUES
+(1, 22, 31, 21, 21222, 66, '2021-05-01');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
+  `id_user` int(40) NOT NULL,
+  `nom_prenom` varchar(50) NOT NULL,
+  `cin` varchar(40) NOT NULL,
+  `N_télé` int(40) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `passwd` varchar(40) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+INSERT INTO `utilisateur` (`id_user`, `nom_prenom`, `cin`, `N_télé`, `email`, `passwd`) VALUES
+(1, 'khalid fifel', 'PA248450', 649459004, 'admin@admin.com', ',kkkj');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id_client`);
+
+--
+-- Indexes for table `entrer`
+--
+ALTER TABLE `entrer`
+  ADD PRIMARY KEY (`id_de_preduit`);
+
+--
+-- Indexes for table `sortie`
+--
+ALTER TABLE `sortie`
+  ADD PRIMARY KEY (`id_de_sortie`);
+
+--
+-- Indexes for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id_client` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `entrer`
+--
+ALTER TABLE `entrer`
+  MODIFY `id_de_preduit` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `sortie`
+--
+ALTER TABLE `sortie`
+  MODIFY `id_de_sortie` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id_user` int(40) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- pages/CL.php
+++ pages/CL.php
@@ -1,3 +1,25 @@
+<?php 
+
+ include("connexion_PDO.php"); ?>
+
+<?php 
+
+ @$nom=$_POST["nom"];
+ @$cin=$_POST["cin"];
+ @$ntele=$_POST["ntele"];
+ @$nom_de_magazine=$_POST["nom_de_magazine"];
+ @$nfix=$_POST["nfix"];
+ @$valider=$_POST["valider"];
+
+if (isset($valider)) {
+    
+    $req="INSERT INTO client(nom_prenom,cin,N_télé,N_fix,nom_de_magazine) VALUES(?,?,?,?,?)";
+    $insert=$conn->prepare($req); 
+    $insert->execute(array($nom,$cin,$ntele,$nfix,$nom_de_magazine));
+    $conn=NULL;
+ 
+} ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -27,10 +49,10 @@ body{
   position: relative;
 }
 .btn{
-  width: 90px;
-  height: 45px;
-  margin-top: 28px;
-  margin-left: 90px;
+  width: 200px;
+  height: 35px;
+  margin-top: 33px;
+  margin-left: 5px;
 }
   </style>
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
@@ -41,45 +63,37 @@ body{
 <body>
 <?php
 ?>
-<!--form ------------------------------------------------>
-<form class="needs-validation" novalidate>
+<!-------------form ------------------------------------->
+<form class="needs-validation" action="" method="post" novalidate>
   <fieldset>
   <h3>Espace de Client :</h3> <br>
   <div class="form-row">
     <div class="col-md-4 mb-3">
-      <label for="validationTooltip01">Nom de client :</label>
-      <input type="text" class="form-control" id="validationTooltip01" placeholder="Nom  ?" value="" required>
-      <div class="valid-tooltip">
-        Looks good!
-      </div>
+      <label for="validationTooltip01">Nom et prenom de client :</label>
+      <input type="text" name="nom" class="form-control" placeholder="Nom  ?" required>
     </div>
     <div class="col-md-4 mb-3">
       <label for="validationTooltip02">CIN de client :</label>
-      <input type="text" class="form-control" id="validationTooltip02" placeholder="Cin ?" value="" required>
-      <div class="valid-tooltip">
-        Looks good!
-      </div>
+      <input type="text" name="cin" class="form-control" placeholder="Cin ?" required>
     </div>
     <div class="col-md-4 mb-3">
       <label for="validationTooltipUsername">N-télé de client :</label>
-      <input type="text" class="form-control" id="validationTooltipUsername" placeholder="N-télé ?" value="" required>
-        <div class="invalid-tooltip">
-          Please choose a unique and valid username.
-        </div>
+      <input type="text" class="form-control" placeholder="N-télé ?" name="ntele" required>
       </div>
     </div>
   </div>
   <div class="form-row">
-    <div class="col-md-3 mb-3">
+    <div class="col-md-4 mb-3">
       <label for="validationTooltip05">Fix de client :</label>
-      <input type="number" class="form-control" id="validationTooltip05" placeholder="Fix ?" required>
+      <input type="number" name="nfix" class="form-control" placeholder="Fix ?" required>
       
     </div>
-    <div class="col-md-3 mb-3">
+    <div class="col-md-4 mb-3">
       <label for="validationTooltip05">Nom de magazine :</label>
-      <input type="text" class="form-control" id="validationTooltip05" placeholder="Nom de magazine ?" required>
+      <input type="text"  class="form-control" name="nom_de_magazine" 
+       placeholder="Nom de magazine ?" required>
     </div>
-    <button class="btn btn-primary" type="submit">Ajoute</button>
+    <input  class="btn btn-primary" name="valider" type="submit" value="Ajoute">
   </div>
   </fieldset>
 </form>
